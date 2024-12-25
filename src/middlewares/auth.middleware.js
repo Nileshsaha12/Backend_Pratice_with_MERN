5852aeba4e0e49b221d4762d1c687a40f2afb1e4
Nileshsaha12 <nileshsaha264@gmail.com>
2024-12-25 10:02:13 +0530

    fix auth middleware function name and enhance user routes with password change, current user retrieval, and watch history features

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -3,7 +3,7 @@ import { ApiError } from "../utils/ApiError.js";
 import jwt from "jsonwebtoken";
 import { User } from "../models/user.module.js";
 
-export const veriftJWT = asyncHandler(async(req,res,
+export const verifyJWT = asyncHandler(async(req,res,
     next)=> {
       try {
          const token =  req.cookies?.accessToken || req.header
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,26 +1,50 @@
 import { Router } from "express";
-import { refreshAccessToken,loggoutUser, loginUser, registerUser } from "../controllers/user.controller.js";
+import {
+  changeCurrentPassword,
+  getCurrentUser,
+  updateaccountDetails,
+  updateUserAvater,
+  updateUserCoverImage,
+  getUserChannelProfile,
+  getWatchHistory,
+  refreshAccessToken,
+  loggoutUser,
+  loginUser,
+  registerUser,
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
-import { veriftJWT } from "../middlewares/auth.middleware.js";
-const router = Router()
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+const router = Router();
 
 router.route("/register").post(
-    upload.fields([
-        {
-            name: "avatar",
-            maxCount: 1
-        },
-        {
-            name: "coverImage",
-            maxCount: 1
-        }
-    ]),
-    registerUser)
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+);
 
-router.route("/login").post(loginUser)
+router.route("/login").post(loginUser);
 
 //secure Routes
 
-router.route("/logout").post(veriftJWT, loggoutUser)
-router.route("/refresh-token").post(refreshAccessToken)
-export default router
+router.route("/logout").post(verifyJWT, loggoutUser);
+router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
+router.route("/update-account").patch(verifyJWT, updateaccountDetails);
+router
+  .route("/avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvater);
+router
+  .route("cover-image")
+  .patch(verifyJWT, upload.single("/coverImage"), updateUserCoverImage);
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/history").get(verifyJWT, getWatchHistory);
+export default router;
